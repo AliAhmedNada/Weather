8f447648b037091df6bfc4a1cc2dbe9d9a006da4
AliAhmedNada <aliahmedalihassan@gmail.com>
2018-02-19 16:04:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 
-import requests
 import json
 import os
 
@@ -51,29 +50,31 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {    
-        url = "https://platform.uipath.com/api/account/authenticate"
-        payload = " {\r\n                            \"tenancyName\" : \"NaneesRefaie\",\r\n                            \"usernameOrEmailAddress\" : \"Asmaa\",\r\n                            \"password\" : \"Advansys@2018\"\r\n                            }"
-        headers = {
-                    'Content-Type': "application/json",
-                     'Cache-Control': "no-cache"
-					}
-        response = requests.request("POST", url, data=payload, headers=headers) 
-        keyAuthenicate=json.loads(response.text)
-        #print("the key is "+keyAuthenicate["result"]);
-        url = "https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs"
-        payload = "{\r\n  \"startInfo\": {\r\n    \"ReleaseKey\": \"923c44bb-81cf-4ddf-895f-538896845d79\",\r\n    \"RobotIds\": [\r\n      12440\r\n    ],\r\n    \"NoOfRobots\": 0,\r\n    \"Strategy\": \"Specific\"\r\n  }\r\n}\r\n "
-        headers = {
-                    'Content-Type': "application/json",
-                    'Authorization': "Bearer "+keyAuthenicate["result"] ,
-                    'Cache-Control': "no-cache"
-					}
+    if req.get("result").get("action") != "accountDetails":
+        return {}
+	
+	url = "https://platform.uipath.com/api/account/authenticate"
+	payload = " {\r\n                            \"tenancyName\" : \"NaneesRefaie\",\r\n                            \"usernameOrEmailAddress\" : \"Asmaa\",\r\n                            \"password\" : \"Advansys@2018\"\r\n                            }"
+	headers = {
+    'Content-Type': "application/json",
+    'Cache-Control': "no-cache"
+    }
 
-                response = requests.request("POST", url, data=payload, headers=headers)
-                print(response.text)
-        }
-    return 1
+	response = requests.request("POST", url, data=payload, headers=headers)
+
+	keyAuthenicate=json.loads(response.text)
+
+	url = "https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs"
+
+	payload = "{\r\n  \"startInfo\": {\r\n    \"ReleaseKey\": \"923c44bb-81cf-4ddf-895f-538896845d79\",\r\n    \"RobotIds\": [\r\n      12440\r\n    ],\r\n    \"NoOfRobots\": 0,\r\n    \"Strategy\": \"Specific\"\r\n  }\r\n}\r\n "
+	headers = {
+    'Content-Type': "application/json",
+    'Authorization': "Bearer "+keyAuthenicate["result"] ,
+    'Cache-Control': "no-cache"
+    }
+
+	response = requests.request("POST", url, data=payload, headers=headers)
+    return res
 
 
 def makeYqlQuery(req):
